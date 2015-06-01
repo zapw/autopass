2844719f7d1db3ad08dadadb5a32fb64bccee6c6
Marcelo Eskinasi Azevedo <marzevd@gmail.com>
2015-06-01 13:41:41 +0000

    changes

--- conf/cookbooks/samba/recipes/default.bash
+++ conf/cookbooks/samba/recipes/default.bash
@@ -5,16 +5,17 @@ if [[ $os_rel = "redhat" ]] ; then
     if (( os_relver == 7 ))  ; then
 	package_install samba
         systemctl enable nmb smb
+	echo "$smb_conf" >"/etc/samba/smb.conf"
 	systemctl restart smb nmb
     elif (( os_relver == 6 )) ; then
 	  package_install samba4
 	  chkconfig nmb on
 	  chkconfig smb on
+	  echo "$smb_conf" >"/etc/samba/smb.conf"
 	  service smb restart
 	  service nmb restart
     fi
 fi
 
 echo "$mkhomedir" >"/usr/local/sbin/mkhomedir"
-echo "$smb_conf" >"/etc/samba/smb.conf"
 chmod +x /usr/local/sbin/mkhomedir
