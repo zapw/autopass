1cf40e6f76154f9270ce700df7055825761c149c
Marcelo Eskinasi Azevedo <marceloe@localhost.localdomain>
2015-06-21 07:43:55 -0400

    changes

--- conf/cookbooks/repliweb/postconnect/postrecipes/distribution.bash
+++ conf/cookbooks/repliweb/postconnect/postrecipes/distribution.bash
@@ -218,7 +218,7 @@ for key in "${!domainsvnpath[@]}"; do
      domain=($(awk -vvar='spotplatform\\.' -F, '{ for (f=1 ; f <= NF ; f++ ) if ( $f ~ var ) print $f }' <<<"${domainsvnpath[$key]}"))
      if (( ${#domain[@]} > 1 )); then
           echo "More than one site with a 'spotplatform.' domain found: ${domain[@]}"
-	  echo "skippking"
+	  echo "skipping"
 	  continue
      elif [[ ! ${domain[@]} ]]; then
               continue
