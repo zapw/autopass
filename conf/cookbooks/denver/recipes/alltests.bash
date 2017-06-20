04ddb25a0c8a90ce8dcefed3ef73d4829c4930ea
Marcelo <marcelo@localhost.local>
2017-06-20 14:47:46 +0300

    asdasd

--- conf/cookbooks/denver/recipes/alltests.bash
+++ conf/cookbooks/denver/recipes/alltests.bash
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-su - "$username"
-cd ~/DenverTraining/services/ || exit 1
+sudo username="$username" runuser -u "$username" -- /bin/bash <<'EOXX'
+cd /home/$username/DenverTraining/services/ || exit 1
 ./run_tests
 
-cd ~/DenverTraining/sv_driver || exit 1
+cd /home/$username/DenverTraining/sv_driver || exit 1
 ./load.sh i40e
 
-cd ~/DenverTraining/Denver/
+cd /home/$username/DenverTraining/Denver/
 bin/Denver  -r bin/GoldenRegression.xml -s bin/GoldenSetup.xml
 
 ./load.sh ixgbe  –link=1gfull
 bin/Denver -r bin/GoldenNianticRegression.xml  -s bin/GoldenNianticSetup.xml
-
-exit 0
+EOXX
